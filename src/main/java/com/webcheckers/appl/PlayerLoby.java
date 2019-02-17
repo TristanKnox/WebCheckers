36de9518421e1b82a214c8373888c2d45073b005
tfk8939 <tfk8939@rit.edu>
2019-02-17 17:32:31 -0500

    Created a method for adding new Players

--- src/main/java/com/webcheckers/appl/PlayerLoby.java
+++ src/main/java/com/webcheckers/appl/PlayerLoby.java
@@ -1,4 +1,33 @@
 package com.webcheckers.appl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This Class holds all of the current userers loged into the WebCheckers App
+ */
 public class PlayerLoby {
+
+    Map<String, PlayerServices> curentUsers;
+
+
+    //Constructor
+    public PlayerLoby(){
+        curentUsers = new HashMap<>();
+    }
+
+    /**
+     * Adds a new user to currentUsers providing the username dose not already exist
+     * @param username is the username that the user is attempting to login with
+     * @return true if user was successfully added
+     * @return false if username was already in use and the user was not added
+     */
+    public boolean addPlayer(String username){
+        if (curentUsers.containsKey(username))
+            return false;   //User name already in use
+        curentUsers.put(username,new PlayerServices());     //username added as Key and a new PlayerService created as Value
+        return true;
+    }
+
+
 }
