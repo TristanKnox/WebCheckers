f79dd7f0b79bbdebf1beccb1f90eb08afa8696e5
Evan Nolan <emn3779@g.rit.edu>
2019-02-23 12:59:58 -0500

    Imported functionality of the model, have not begun full integration/ implementation of model

--- src/main/java/com/webcheckers/appl/GameCenter.java
+++ src/main/java/com/webcheckers/appl/GameCenter.java
@@ -1,19 +1,21 @@
 package com.webcheckers.appl;
 
+import com.webcheckers.model.checkers.Game;
+
 import java.util.HashMap;
 
 public class GameCenter {
 
     //todo spawn game and what not.  Hold list of players like hashmap of <String, Games> and active games
     //todo spawn a game based off of two player objs. Return Game.
-//    private HashMap<String, Game> activeGames; //player name to Game obj
+    private HashMap<String, Game> activeGames; //player name to Game obj
     private HashMap<Player, Player> playersInMatch; //player to player for matches
 
     /**
      * creates the gamecenter object for the server application.
      */
     public GameCenter(){
-//        activeGames= new HashMap<String, Game>();
+        activeGames= new HashMap<String, Game>();
         playersInMatch = new HashMap<>();
     }
 
@@ -23,10 +25,10 @@ public class GameCenter {
      * @param playerTwo
      * @return
      */
-//    public Game spawnGame(Player playerOne, Player playerTwo){
-//        //todo fill in with the game constructor. and return the following game
-//        return null;
-//    }
+    public Game spawnGame(Player playerOne, Player playerTwo){
+        //todo fill in with the game constructor. and return the following game
+        return null;
+    }
 
     /**
      * Adds players to game to keep track of them.
@@ -44,20 +46,20 @@ public class GameCenter {
      * @param player the player
      * @param game the game instance.
      */
-//    public void addPlayerToGame(Player player, Game game){
-//        activeGames.put(player.getPlayer(), game);
-//    }
-//
-//
-//    /**
-//     * gets the other player of the game.
-//     * @param player player whose opponent wants to be found.
-//     * @return the players opponent, if it exists.
-//     */
-//    public Player getOtherPlayer(Player player) {
-//       return  playersInMatch.containsKey(player) ? playersInMatch.get(player) : null;
-//    }
-//    public Game getGaame(String username){
-//        return activeGames.get(username);
-//    }
+    public void addPlayerToGame(Player player, Game game){
+        activeGames.put(player.getPlayer(), game);
+    }
+
+
+    /**
+     * gets the other player of the game.
+     * @param player player whose opponent wants to be found.
+     * @return the players opponent, if it exists.
+     */
+    public Player getOtherPlayer(Player player) {
+       return  playersInMatch.containsKey(player) ? playersInMatch.get(player) : null;
+    }
+    public Game getGaame(String username){
+        return activeGames.get(username);
+    }
 }
