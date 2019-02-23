c91950887e8a25527c6670cb2bb1cb960f014197
Collin Bolles <collinbolles@gmail.com>
2019-02-22 19:52:52 -0500

    package: model Created basic model classes for webcheckers

---
+++ src/main/java/com/webcheckers/model/checkers/Game.java
@@ -0,0 +1,64 @@
+package com.webcheckers.model.checkers;
+
+import com.webcheckers.model.Player;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * The Game class represents a single checkers game that takes place between two players.
+ * It keeps tracks of the state of the board and the players involved
+ *
+ * @author Collin Bolles
+ */
+public class Game implements Iterable<Row> {
+
+  /** Max 8 rows per board **/
+  private static final int MAX_SIZE = 8;
+
+  /** The player initiating the game **/
+  private Player playerOne;
+  /** The player selected to play the game **/
+  private Player playerTwo;
+  /** Represents each row of the board **/
+  private List<Row> rows;
+
+  /**
+   * Creates an initial game with the rows initialized each player kept track of
+   * @param playerOne The player to start the game
+   * @param playerTwo The player invited to play the game
+   */
+  public Game(Player playerOne, Player playerTwo) {
+    this.playerOne = playerOne;
+    this.playerTwo = playerTwo;
+    rows = new ArrayList<>();
+    initializeRows();
+  }
+
+  /**
+   * Handles initializing the boards. Each row initializes its own state
+   */
+  private void initializeRows() {
+    for(int row = 0; row < MAX_SIZE; row++) {
+      rows.add(new Row(row));
+    }
+  }
+
+  /**
+   * The iterator method required for the client UI. The iterator returns the iterator
+   * from the list of rows
+   * @return Iterator from the list of rows
+   */
+  @Override
+  public Iterator<Row> iterator() {
+    return rows.iterator();
+  }
+
+  public Player getPlayerOne() {
+    return playerOne;
+  }
+
+  public Player getPlayerTwo() {
+    return playerTwo;
+  }
+}
