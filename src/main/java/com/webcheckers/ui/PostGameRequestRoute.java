b8f9466aa34d17f67ca09a881fc4ef6361cc6f6e
tfk8939 <tfk8939@rit.edu>
2019-02-24 15:27:16 -0500

    Started finnishing the TODO's in PostGameRequestRoute

--- src/main/java/com/webcheckers/ui/PostGameRequestRoute.java
+++ src/main/java/com/webcheckers/ui/PostGameRequestRoute.java
@@ -36,19 +36,19 @@ public class PostGameRequestRoute implements Route {
     //Get players names
     final Session session = request.session();
     //TODO make sure that session was started in getHomeRoute and get access to PlayerServisKey
-    // Player player1 = session.attribute("PlayerServecisKey");
+     Player playerOne = session.attribute(GetHomeRoute.PLAYER_KEY);
     //TODO make sure that the opponet name was stored on post request and get key
-    String player2name = request.queryParams("otherUser");
+    String playerTwoName = request.queryParams("otherUser");
 
 
     //Remove players from playerLobby
-    //TODO Make sure that the PlayerLobby way to remove a player using its username from the avialable player list and return that player
-    //    playerOne = playerLobby.getPlayer(player1.getName());
-    //    Player playerTwo = playerLobby.getPlayer(payer2name);
+    //TODO Make sure that the PlayerLobby way to remove a player using its username from the avialable player list and return that play
+      playerOne = playerLobby.getPlayerForGame(playerOne.getName());
+      Player playerTwo = playerLobby.getPlayerForGame(playerTwoName);
 
     //Inject players into game center to retrev game
     //TODO insure that GameCenter exissts and has a startGame method that takes 2 players and returns a game
-    Game game = gameCenter.getGame(player1,player2);
+    Game game = gameCenter.getGame(playerOne,playerTwo);
 
 
     Map<String, Object> vm = new HashMap<>();
@@ -62,16 +62,11 @@ public class PostGameRequestRoute implements Route {
     // Fake view mode
     vm.put("viewMode", "PLAY");
     // Fake board
-    vm.put("board", ViewGenorator.getView(game,playerOne));
+    vm.put("board", ViewGenorator.getView(game, playerOne));
 
 
     // render the View
     return templateEngine.render(new ModelAndView(vm , "game.ftl"));
 
-
-
-
-
-    return null;
   }
 }
