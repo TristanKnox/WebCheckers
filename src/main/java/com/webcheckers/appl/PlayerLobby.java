70be3f491f2e81284a35a48f33324ee5025f2823
jjirinec <43682906+jjirinec@users.noreply.github.com>
2019-02-24 15:28:49 -0500

    Format Fix

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -15,7 +15,7 @@ public class PlayerLobby {
 
 
     //Constructor
-    public PlayerLobby(){
+    public PlayerLobby() {
         currentUsers = new HashMap<>();
         inGameUsers = new HashMap<>();
         avalUsers = new HashMap<>();
@@ -23,12 +23,12 @@ public class PlayerLobby {
 
     /**
      * Adds a new user to currentUsers providing the username dose not already exist
+     *
      * @param username is the username that the user is attempting to login with
-     * @return true if user was successfully added
      * @return false if username was already in use and the user was not added
      */
-    public synchronized boolean addPlayer(String username){
-        if(isValidUsername(username)) {
+    public synchronized boolean addPlayer(String username) {
+        if (isValidUsername(username)) {
             Player temp = new Player(username);
             currentUsers.put(username, temp);
             avalUsers.put(username, temp);
@@ -40,29 +40,25 @@ public class PlayerLobby {
 
     /**
      * Checks to see if a username is valid or not
+     *
      * @param username in question
-     * @return true if username is valid
      * @return false if username is invalid
      */
-    public boolean isValidUsername(String username){
-        if (!currentUsers.containsKey(username)){
-            if(username.length() == 0) {
+    public boolean isValidUsername(String username) {
+        if (!currentUsers.containsKey(username)) {
+            if (username.length() == 0) {
                 return false;
-            }
-            else if (!Character.isLetter(username.charAt(0))){
+            } else if (!Character.isLetter(username.charAt(0))) {
                 return false;
-            }
-            else{
-                for (int i = 0; i < username.length(); i++){
+            } else {
+                for (int i = 0; i < username.length(); i++) {
                     char c = username.charAt(i);
                     int ascii = (int) username.charAt(i);
-                    if(c == ' '){
+                    if (c == ' ') {
                         continue;
-                    }
-                    else if((48 <= ascii && ascii <=57) || (65 <= ascii && ascii <=90) || (97 <= ascii && ascii <=122)) {
+                    } else if ((48 <= ascii && ascii <= 57) || (65 <= ascii && ascii <= 90) || (97 <= ascii && ascii <= 122)) {
                         continue;
-                    }
-                    else{
+                    } else {
                         return false;
                     }
                 }
@@ -84,9 +80,10 @@ public class PlayerLobby {
 
     /**
      * Creates a list of usernames repersenting all curently loged in users
+     *
      * @return the list of usernames
      */
-    public List<Player> getAllAvalPlayers(){
+    public List<Player> getAllAvalPlayers() {
         Collection<Player> players = avalUsers.values();
         List<Player> users = new ArrayList<>(players);
         return users;
@@ -94,31 +91,31 @@ public class PlayerLobby {
 
     /**
      * This will remove the given user from currentUsers
+     *
      * @param username the user name
-     * @return true if successful
      * @return false if unsuccessful
      */
-    public synchronized boolean logout(String username){
-        if(currentUsers.containsKey(username)){
+    public synchronized boolean logout(String username) {
+        if (currentUsers.containsKey(username)) {
             currentUsers.remove(username);
             avalUsers.remove(username);
             return true;
-        }
-        else
+        } else
             return false;
     }
 
-    public Player getPlayer(String username){
-        return(avalUsers.get(username));
+    public Player getPlayer(String username) {
+        return (avalUsers.get(username));
     }
 
-    public void removePlayer(String username){
+    public void removePlayer(String username) {
         avalUsers.remove(username);
     }
 
-    public Player getPlayerForGame(String username){
+    public Player getPlayerForGame(String username) {
         Player ret = getPlayer(username);
         removePlayer(username);
         return ret;
     }
+}
 
