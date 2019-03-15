b4288f28284568d9ae42a926b5b8e90b2c4dccd5
tfk8939 <tfk8939@rit.edu>
2019-03-15 17:34:23 -0400

    unit-testing for Row

---
+++ src/test/java/com/webcheckers/ui/model/RowTest.java
@@ -0,0 +1,57 @@
+package com.webcheckers.ui.model;
+
+import com.webcheckers.model.checkers.Row;
+import com.webcheckers.model.checkers.Space;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+@Tag("Model-tier")
+public class RowTest {
+
+    private final int INDEX = 5;
+    private final List<Space> spaces = new ArrayList<Space>(8);
+
+
+
+    @BeforeEach
+    public void setup(){
+        for(Space space : spaces){
+            space = mock(Space.class);
+        }
+    }
+
+    @Test
+    public void indexConstructor(){
+        Row row = new Row(INDEX);
+        assertEquals(INDEX,row.getIndex(),"The index of row was expected to be " + INDEX + " but was " + row.getIndex());
+        assertNotNull(row.getSpaces());
+    }
+
+    @Test
+    public void indexAndSpacesConstructor(){
+        Row row = new Row(spaces, INDEX);
+        assertEquals(INDEX,row.getIndex(),"The index of row was expected to be " + INDEX + " but was " + row.getIndex());
+        assertNotNull(row.getSpaces());
+        assertEquals(spaces,row.getSpaces());
+    }
+
+    @Test
+    public void spaceInitilazation(){
+        Row row;
+        for(int index = 0 ;  index < 8 ; index++){
+            row = new Row(index);
+            for(Space space: row.getSpaces()){
+                if(space.)
+            }
+        }
+    }
+
+}
--- src/test/java/com/webcheckers/ui/ui/PostGameRequestRouteTest.java
+++ src/test/java/com/webcheckers/ui/ui/PostGameRequestRouteTest.java
@@ -4,29 +4,33 @@ package com.webcheckers.ui.ui;
 import com.webcheckers.appl.GameCenter;
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Player;
+import com.webcheckers.model.checkers.Game;
+import com.webcheckers.model.checkers.Piece;
+import com.webcheckers.ui.GetGameRoute;
+import com.webcheckers.ui.GetHomeRoute;
 import com.webcheckers.ui.PostGameRequestRoute;
+import com.webcheckers.ui.TemplateEngineTester;
+import com.webcheckers.ui.ViewObjects.ViewGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import spark.Request;
-import spark.Response;
-import spark.Session;
-import spark.TemplateEngine;
+import spark.*;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @Tag("UI-tier")
 public class PostGameRequestRouteTest {
 
     private static final String POSTED_USER_NAME = "Tom";
+    private static final String PLAYER1_USER_NAME = "Bob";
     private static final String POSTED_USER =   "otherUser";
 
     private PostGameRequestRoute CuT;
 
     // friendly objects
-    private GameCenter gameCenter;
-    private PlayerLobby playerLobby;
+
 
     // attributes holding mock objects
     private Request request;
@@ -38,6 +42,10 @@ public class PostGameRequestRouteTest {
     //player2 represents the user that player1 is requesting a game with
     private Player player2;
 
+    private GameCenter gameCenter;
+    private PlayerLobby playerLobby;
+    private Game game;
+
 
     @BeforeEach
     public void setup(){
@@ -45,24 +53,73 @@ public class PostGameRequestRouteTest {
         session = mock(Session.class);
         when(request.session()).thenReturn(session);
 
+
         response = mock(Response.class);
         engine = mock(TemplateEngine.class);
+
+        game = mock(Game.class);
+        gameCenter = mock(GameCenter.class);
+        playerLobby = mock(PlayerLobby.class);
+
+
 
         player1 = mock(Player.class);
         player2 = mock(Player.class);
         when(player2.getName()).thenReturn(POSTED_USER_NAME);
+        when(player1.getName()).thenReturn(PLAYER1_USER_NAME);
+
+        when(request.queryParams(POSTED_USER)).thenReturn(POSTED_USER_NAME);
+        when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(player1);
+
+        CuT = new PostGameRequestRoute(engine,playerLobby,gameCenter);
     }
 
 
     @Test
     public void player2_not_available(){
-        when(request.queryParams(POSTED_USER)).thenReturn(POSTED_USER_NAME);
-        playerLobby = mock(PlayerLobby.class);
         when(playerLobby.getPlayer(POSTED_USER_NAME)).thenReturn(player2);
         when(playerLobby.isInGame(player2)).thenReturn(true);
 
+      //  final TemplateEngineTester testHelper = new TemplateEngineTester();
+       // when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
 
+        assertThrows(HaltException.class, () -> CuT.handle(request,response));
 
+       // CuT.handle(request,response);
+       // assertTrue(session.attribute(GetHomeRoute.IN_GAME_ERROR_FLAG));
+       // testHelper.assertViewModelExists();
+       // testHelper.assertViewModelIsaMap();
+        //testHelper.assertViewModelAttribute(GetHomeRoute.IN_GAME_ERROR_FLAG,true);//TODO figure out how to check for this after exception is thrown
+    }
+
+    @Test
+    public void game_started(){
+        when(playerLobby.getPlayer(POSTED_USER_NAME)).thenReturn(player2);
+        when(playerLobby.isInGame(player2)).thenReturn(false);
+
+        when(playerLobby.getPlayerForGame(player1.getName())).thenReturn(player1);
+        when(playerLobby.getPlayerForGame(POSTED_USER_NAME)).thenReturn(player2);
+        when(game.getRedPlayer()).thenReturn(player1);
+        when(game.getWhitePlayer()).thenReturn(player2);
+        when(game.getActivateColor()).thenReturn(Piece.PieceColor.RED);
+
+        when(gameCenter.spawnGame(player1,player2)).thenReturn(game);
+
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        CuT.handle(request,response);
+
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+
+        testHelper.assertViewModelAttribute("title", GetGameRoute.GAME_TITLE);
+        testHelper.assertViewModelAttribute("currentUser", player1);
+        testHelper.assertViewModelAttribute("redPlayer", player1);
+        testHelper.assertViewModelAttribute("whitePlayer", player2);
+        testHelper.assertViewModelAttribute("activeColor", Piece.PieceColor.RED);
+        testHelper.assertViewModelAttribute("viewMode", "PLAY");
+        testHelper.assertViewModelAttribute("board", ViewGenerator.getView(game,game.getPlayerColor(player1)));
     }
 
 
