2856f285df51a9f8f9ade7f3cee9b84a1bcea42a
Evan Nolan <emn3779@g.rit.edu>
2019-03-17 23:21:51 -0400

    Started PostSignInAttemptTest, currently stuck

---
+++ src/test/java/com/webcheckers/ui/PostSignInAttemptTest.java
@@ -0,0 +1,54 @@
+package com.webcheckers.ui;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.webcheckers.appl.PlayerLobby;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spark.ModelAndView;
+import spark.Response;
+import spark.Request;
+import spark.Session;
+import spark.TemplateEngine;
+import org.junit.jupiter.api.Tag;
+
+@Tag("UI-tier")
+public class PostSignInAttemptTest {
+
+  private static final String USERNAME = "Username";
+  private static final String TEST_NAME_VALID = "Timmy";
+  private static final String TEST_NAME_VALID2 = "Mumps";
+  private static final String TEST_NAME_VALID3 = "Shingles";
+  private static final String TEST_NAME_VALID4 = "Sh1nGL3Z";
+  private static final String TEST_NAME_INVALID = "1gangnamstyletogoplease";
+  private static final int EMPTY = 0;
+
+  private PostSignInAttemptRoute CuT;
+
+  private Request request;
+  private Session session;
+  private Response response;
+  private TemplateEngine templateEngine;
+  private PlayerLobby playerLobby;
+
+  @BeforeEach
+  public void setup(){
+    request = mock(Request.class);
+    session = mock(Session.class);
+    when(request.session()).thenReturn(session);
+
+    response = mock(Response.class);
+    templateEngine = mock(TemplateEngine.class);
+
+    playerLobby = mock(PlayerLobby.class);
+  }
+
+  @Test
+  public void test_Login_Query(){
+    when(request.queryParams())
+  }
+
+}
