6ca9c0a6be76239a8288dd53adc46269d0481c67
Collin Bolles <collinbolles@gmail.com>
2019-03-22 20:41:36 -0400

    Add equals methods to Move and Position

--- src/main/java/com/webcheckers/model/checkers/Position.java
+++ src/main/java/com/webcheckers/model/checkers/Position.java
@@ -10,6 +10,16 @@ public class Position {
         this.cell = cell;
     }
 
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj instanceof Position) {
+            Position other = (Position) obj;
+            return this.row == other.row && this.cell == other.cell;
+        }
+        return false;
+    }
+
     public int getRow(){ return row; }
     public int getCell(){ return cell; }
 
