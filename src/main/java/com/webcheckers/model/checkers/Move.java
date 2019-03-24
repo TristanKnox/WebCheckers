da899edb4c143d2fe39a0ffccd50545696979761
tfk8939 <tfk8939@rit.edu>
2019-03-24 17:13:46 -0400

    Added commetns to Move

--- src/main/java/com/webcheckers/model/checkers/Move.java
+++ src/main/java/com/webcheckers/model/checkers/Move.java
@@ -1,20 +1,55 @@
 package com.webcheckers.model.checkers;
 
+/**
+ * This class repersents a move from one position to another
+ */
 public class Move {
 
-    private Position start;
-    private Position end;
+  private Position start;
+  private Position end;
 
-    public Move(Position start, Position end){
-        this.start = start;
-        this.end = end;
+  /**
+   * Constructor - creates a new Move object
+   * @param start - the position the piece is moving from
+   * @param end - the position the piece is moving to
+   */
+  public Move(Position start, Position end){
+    this.start = start;
+    this.end = end;
+  }
+
+  /**
+   * Getter method for the start position
+   * @return - start position
+   */
+  public Position getStart(){ return start; }
+
+  /**
+   * Getter method for end position
+   * @return - end position
+   */
+  public Position getEnd(){ return end; }
+
+  /**
+   * Checks to see if this instance of Move is equivilant ot the given object
+   * @param obj - the object to compare with this
+   * @return true if equal false if not equal
+   */
+  public boolean equals(Object obj) {
+    if(this == obj)
+      return true;
+    if(obj instanceof Position) {
+      Move other = (Move) obj;
+      return this.start == other.start && this.end == other.end;
     }
+    return false;
+  }
 
-
-    public Position getStart(){ return start; }
-    public Position getEnd(){ return end; }
-
-    public String toString(){
-        return "Start: " + start.toString() + "\nEnd: " + end.toString();
-    }
+  /**
+   * overriden toString
+   * @return - the string that represents this object
+   */
+  public String toString(){
+    return "Start: " + start.toString() + "\nEnd: " + end.toString();
+  }
 }
--- src/main/java/com/webcheckers/ui/PostMoveRequestRoute.java
+++ src/main/java/com/webcheckers/ui/PostMoveRequestRoute.java
@@ -16,36 +16,36 @@ import java.net.URLDecoder;
  */
 public class PostMoveRequestRoute implements Route {
 
-    TemplateEngine templateEngine;
-    GameCenter gameCenter;
+  TemplateEngine templateEngine;
+  GameCenter gameCenter;
 
-    public PostMoveRequestRoute(TemplateEngine templateEngine, GameCenter gameCenter){
-        this.templateEngine = templateEngine;
-        this.gameCenter = gameCenter;
-    }
-    @Override
-    public Object handle(Request request, Response response) throws Exception {
-        Gson gson = new Gson();
-        Session session = request.session();
-        //Get the player
-        Player player = session.attribute(GetHomeRoute.PLAYER_KEY);
-        //Get the game associated with the player
-        Game game = gameCenter.getGame(player);
-        //Get the posted move
-        String moveJson = request.body();
-        //Decode the moveJson to a usable formate
-        moveJson = URLDecoder.decode(moveJson,"UTF-8");
+  public PostMoveRequestRoute(TemplateEngine templateEngine, GameCenter gameCenter){
+    this.templateEngine = templateEngine;
+    this.gameCenter = gameCenter;
+  }
+  @Override
+  public Object handle(Request request, Response response) throws Exception {
+    Gson gson = new Gson();
+    Session session = request.session();
+    //Get the player
+    Player player = session.attribute(GetHomeRoute.PLAYER_KEY);
+    //Get the game associated with the player
+    Game game = gameCenter.getGame(player);
+    //Get the posted move
+    String moveJson = request.body();
+    //Decode the moveJson to a usable formate
+    moveJson = URLDecoder.decode(moveJson,"UTF-8");
 
-        //Convert the JsonMove to a Move object
-        Move move = gson.fromJson(moveJson.substring(11),Move.class);
-        //Validats the movee
-        Turn.TurnResponse moveResponce = game.addMove(move);
-        //Sets up the mesage to be returnd
-        Message msg;
-        if(moveResponce == Turn.TurnResponse.VALID_TURN)
-            msg = Message.info("Move is good");
-        else
-            msg = Message.error("Invalid move: " + moveResponce);
-        return gson.toJson(msg);
-    }
+    //Convert the JsonMove to a Move object
+    Move move = gson.fromJson(moveJson.substring(11),Move.class);
+    //Validats the movee
+    Turn.TurnResponse moveResponce = game.addMove(move);
+    //Sets up the mesage to be returnd
+    Message msg;
+    if(moveResponce == Turn.TurnResponse.VALID_TURN)
+      msg = Message.info("Move is good");
+    else
+      msg = Message.error("Invalid move: " + moveResponce);
+    return gson.toJson(msg);
+  }
 }
