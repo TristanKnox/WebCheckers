8de3ad4d70b390e62b83f31ff11eb89ce44e8e04
Evan Nolan <emn3779@g.rit.edu>
2019-03-27 17:15:09 -0400

    Added unit testing to sign out route

--- src/test/java/com/webcheckers/ui/PostResignationRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostResignationRouteTest.java
@@ -1,4 +1,35 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.appl.GameCenter;
+import com.webcheckers.appl.PlayerLobby;
+
+
+import com.google.gson.Gson;
+
+import com.webcheckers.appl.PlayerLobby;
+
+
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class PostResignationRouteTest {
+  private PostResignationRoute CuT;
+
+  //mock objects
+  private Request request;
+  private Session session;
+
+  private GameCenter gameServices;
+  private PlayerLobby playerLobby;
+  private String playerName = "KAKAKAK";
+  private String playerName2 = "Do you think you can dance";
+
+  private String mes_str;
+  private Gson gson;
+
+
 }
