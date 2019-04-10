d4b2baaeb14017c3907e4f2e57b90ec564a498a4
Evan Nolan <emn3779@g.rit.edu>
2019-04-10 18:36:33 -0400

    Trying to get that succulent tests

--- src/test/java/com/webcheckers/ui/TestWebServer.java
+++ src/test/java/com/webcheckers/ui/TestWebServer.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 
 import com.google.gson.Gson;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import spark.TemplateEngine;
@@ -12,10 +13,13 @@ public class TestWebServer {
   private TemplateEngine templateEngine;
   private Gson gson;
   private WebServer CuT;
+  private Logger LOG;
   @BeforeEach
   public void setUp(){
     templateEngine = mock(TemplateEngine.class);
     gson = new Gson();
+    CuT = new WebServer(templateEngine,gson);
+    LOG = new Logger(Logger.getLogger(WebServer.class.getName()));
   }
   @Test
   public void testCtor(){
@@ -23,6 +27,11 @@ public class TestWebServer {
     gson = new Gson();
     WebServer webServer = new WebServer(templateEngine,gson);
     assertNotNull(webServer);
+  }
+  @Test
+  public void testInitialize(){
+    CuT.initialize();
+
   }
 
 }
