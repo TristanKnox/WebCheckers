ae2170e1f6d29d153a7bf8f70dd6f37603a6c244
Evan Nolan <emn3779@g.rit.edu>
2019-04-12 17:51:06 -0400

    Small additions

--- src/main/java/com/webcheckers/ui/ReplayRoutes/PostPreviousTurnRoute.java
+++ src/main/java/com/webcheckers/ui/ReplayRoutes/PostPreviousTurnRoute.java
@@ -1,10 +1,17 @@
 package com.webcheckers.ui.ReplayRoutes;
 
+import com.webcheckers.appl.ReplayCenter;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class PostPreviousTurnRoute implements Route {
+
+  private ReplayCenter replayCenter;
+
+  public PostPreviousTurnRoute(ReplayCenter rCenter){
+    replayCenter = rCenter;
+  }
 
   @Override
   public Object handle(Request request, Response response) throws Exception {
